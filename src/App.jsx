edaf354667a584b900c450793e5a9c3080405eed
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2024-11-20 16:13:50 +0100

    done with navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ResponsiveAppBar from './component/navbar'
+import './App.css'
+
+function App() {
+  return (
+    <div className='back'>
+      <ResponsiveAppBar />
+    </div>
+  )
+}
+
+export default App
