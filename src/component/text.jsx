2ee9b2f5b36c9b4a38800380ba18d8f49e4273b6
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2024-11-20 23:28:36 +0100

    some changes made

--- src/component/text.jsx
+++ src/component/text.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Typography, Button } from '@mui/material';
+import '../App.css'
 
 function TextBig() {
     return (
         <>
             <Box sx={{ color: '#FAFAFA', textAlign: 'center', fontFamily: 'Inter, sans-serif', display: 'flex', justifyContent: 'center' }}>
-                <Typography sx={{ width: { xs: '100%', sm: '100%', md: '50%' }, fontSize: { xs: '25px', sm: '30px', md: '40px' } }}>Ace Your Next Interview with our AI-Powered Mock Sessions</Typography>
+                <Typography sx={{ width: { xs: '100%', sm: '100%', md: '50%' }, fontSize: { xs: '25px', sm: '30px', md: '40px' } }}>Ace Your Next <span className="inter">Interview</span> with our AI-Powered Mock Sessions</Typography>
             </Box>
             <br></br>
             <Box sx={{ color: '#FAFAFA', textAlign: 'center', fontFamily: 'Inter, sans-serif', display: 'flex', justifyContent: 'center' }}>
